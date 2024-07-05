877d7529d769458dd94670823b739be5b5dc2f5c
RFClimber <choiwoolim98@gmail.com>
2024-07-05 13:42:52 +0900

    UI 틀 제작 완료

--- teamProjectCafeHooligan/teamProjectCafeHooligan/Data.swift
+++ teamProjectCafeHooligan/teamProjectCafeHooligan/Data.swift
@@ -1,19 +1,22 @@
-//
-//  Data.swift
-//  teamProjectCafeHooligan
-//
-//  Created by mac on 7/4/24.
-//
 
-import Foundation
 import UIKit
 
+struct MenuItem {
+    let imageName: String
+    let menuName: String
+    let menuPrice: Double
+}
+
+
+extension MenuItem{
+    
+    static let recommended_Menu = [
+        MenuItem(imageName: "cup.and.saucer", menuName: "coffee", menuPrice: 1.3),
+        MenuItem(imageName: "star", menuName: "cappuccino", menuPrice: 1.0),
+        MenuItem(imageName: "cup.and.saucer", menuName: "cafemocha", menuPrice: 0.8),
+        MenuItem(imageName: "star", menuName: "peachtea", menuPrice: 1.4),
+    ]
+}
 struct Category {
     let name: String
 }
-
-struct MenuItem {
-    let name: String
-    let price: Int
-}
- 
--- teamProjectCafeHooligan/teamProjectCafeHooligan/ViewController.swift
+++ teamProjectCafeHooligan/teamProjectCafeHooligan/ViewController.swift
@@ -8,119 +8,141 @@
 import UIKit
 
 class ViewController: UIViewController {
-    //컬렉션뷰 생성
-    let menuCollectionView: UICollectionView = UICollectionView()
     
-    let modeToggleButton = UIButton(type: .system)
-    // B. 시스템모드에 따른 토글 아이콘 사이즈
-    let sunImage = UIImage(systemName: "sun.max.fill")?.withConfiguration(UIImage.SymbolConfiguration(pointSize: 16))
-    let moonImage = UIImage(systemName: "moon.fill")?.withConfiguration(UIImage.SymbolConfiguration(pointSize: 16))
+    let categories = ["coffeeCup", "drink", "bites", "pizza", "cookie"]
+    let categoryTitles = ["Coffee Cup", "Drink", "Bites", "Pizza", "Cookie"]
+    let categoriesImage = ["cup.and.saucer", "wineglass", "carrot", "fork.knife", "birthday.cake"]
     
+    // 컬렉션뷰 생성
+    let menuCollectionView: UICollectionView = {
+    
+        let layout = UICollectionViewFlowLayout()
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        // 속성
+        collectionView.backgroundColor = #colorLiteral(red: 0.9118670225, green: 0.9118669629, blue: 0.9118669629, alpha: 1)
+        
+        
+        return collectionView
+    }()
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        //배경화면
-        view.backgroundColor = .systemBackground
         
         
-        // B. 로고 이미지
-        let logoImageView = UIImageView(image: UIImage(named: "logo_dark"))
-        logoImageView.contentMode = .scaleAspectFit
-        logoImageView.layer.cornerRadius = 8
+
+        // Create a scroll view
+        let scrollView = UIScrollView()
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        scrollView.showsHorizontalScrollIndicator = false
+        scrollView.backgroundColor = #colorLiteral(red: 0.9118670225, green: 0.9118669629, blue: 0.9118669629, alpha: 1)
+        self.view.addSubview(scrollView)
+
+        NSLayoutConstraint.activate([
+            scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            scrollView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 80),
+            scrollView.heightAnchor.constraint(equalToConstant: 55)
+        ])
+
+        // Create a stack view
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.spacing = 10
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        scrollView.addSubview(stackView)
+
+        NSLayoutConstraint.activate([
+            stackView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
+            stackView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
+            stackView.centerYAnchor.constraint(equalTo: scrollView.centerYAnchor),
+            stackView.heightAnchor.constraint(equalToConstant: 32)
+        ])
+
+        // Add buttons to the stack view
+        for i in 0..<categoriesImage.count {
+            let button = createCategoryButton(imageName: categoriesImage[i], title: categoryTitles[i])
+            stackView.addArrangedSubview(button)
+        }
         
-        // B. 로고명 레이블
-        let logoLabel = UILabel()
-        logoLabel.text = "Café Hooligan"
-        logoLabel.font = UIFont.systemFont(ofSize: 24, weight: .medium)
-        logoLabel.textColor = .label
+        menuCollectionView.delegate = self
+        menuCollectionView.dataSource = self
         
-        // B. 다크 모드/라이트 모드 전환 버튼
-        modeToggleButton.addTarget(self, action: #selector(toggleMode), for: .touchUpInside)
+        view.addSubview(menuCollectionView)
         
-        // 아이콘 설정
-        let sunImage = UIImage(systemName: "sun.max.fill")
-        let tintedSunImage = sunImage?.withTintColor(.systemGray, renderingMode: .alwaysOriginal)
-        modeToggleButton.setImage(tintedSunImage, for: .normal)
-        modeToggleButton.layer.cornerRadius = 8
-        
-        // B. 토글 버튼 초기 이미지 설정
-        updateButtonImage()
-        
-        // B. horizontalStackView 생성
-        let horizontalStackView = UIStackView(arrangedSubviews: [logoImageView, logoLabel, modeToggleButton])
-        horizontalStackView.axis = .horizontal
-        horizontalStackView.alignment = .center
-        horizontalStackView.spacing = 10
-        
-        // B. horizontalStackView를 뷰에 추가
-        view.addSubview(horizontalStackView)
-        
-        // B. containerView 생성
-        let containerView = UIView()
-        containerView.backgroundColor = #colorLiteral(red: 0.9118670225, green: 0.9118669629, blue: 0.9118669629, alpha: 1)
-        
-        // B. containerView에 horizontalView 추가
-        containerView.addSubview(horizontalStackView)
-        
-        // B. ContainerView를 뷰에 추가
-        view.addSubview(containerView)
-        
-        // B. Auto Layout 설정
-        horizontalStackView.translatesAutoresizingMaskIntoConstraints = false
-        containerView.translatesAutoresizingMaskIntoConstraints = false
+        menuCollectionView.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
-            
-            // Container View 제약 설정
-            containerView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0),
-            containerView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0),
-            containerView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0),
-            containerView.heightAnchor.constraint(equalToConstant: 40),
-            
-            // Horizontal Stack View 제약 설정
-            horizontalStackView.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 17),
-            horizontalStackView.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -21),
-            horizontalStackView.topAnchor.constraint(equalTo: containerView.topAnchor, constant: 0),
-            horizontalStackView.bottomAnchor.constraint(equalTo: containerView.bottomAnchor, constant: 0),
-            
-            // B. Logo Image 제약 설정
-            logoImageView.widthAnchor.constraint(equalToConstant: 40),
-            logoImageView.heightAnchor.constraint(equalToConstant: 40),
-            modeToggleButton.widthAnchor.constraint(equalToConstant: 40),
-            modeToggleButton.heightAnchor.constraint(equalToConstant: 40)
+            menuCollectionView.topAnchor.constraint(equalTo: scrollView.bottomAnchor),
+            menuCollectionView.leftAnchor.constraint(equalTo: view.leftAnchor),
+            menuCollectionView.rightAnchor.constraint(equalTo: view.rightAnchor),
+            menuCollectionView.bottomAnchor.constraint(equalTo: menuCollectionView.topAnchor, constant: 473)
         ])
-    }
-    
-    @objc func toggleMode() {
-        // B. 다크 모드/라이트 모드 전환 코드
-        if traitCollection.userInterfaceStyle == .light {
-            overrideUserInterfaceStyle = .dark
-        } else {
-            overrideUserInterfaceStyle = .light
-        }
-    }
-    
-    // 사용자 인터페이스 색상 모드 변화 감지, 이전 색상 모드와 현재 색상 모드가 다를 경우에만 실행
-    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
-        super.traitCollectionDidChange(previousTraitCollection)
         
-        // 현재 인터페이스 스타일에 따라 버튼 이미지 업데이트
-        if traitCollection.hasDifferentColorAppearance(comparedTo: previousTraitCollection) {
-            updateButtonImage()
-        }
+        
+        menuCollectionView.register(MenuItemCell.self, forCellWithReuseIdentifier: "MenuCell")
+        
+    }
+    func createCategoryButton(imageName: String, title: String) -> UIButton {
+        let button = UIButton(type: .system)
+        button.backgroundColor = .white
+        button.layer.cornerRadius = 10
+        button.layer.masksToBounds = true
+        button.translatesAutoresizingMaskIntoConstraints = false
+        
+        // Image
+        let imageView = UIImageView(image: UIImage(systemName: imageName))
+        imageView.contentMode = .scaleAspectFit
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.widthAnchor.constraint(equalToConstant: 32).isActive = true
+        imageView.heightAnchor.constraint(equalToConstant: 32).isActive = true
+        
+        // Title
+        let label = UILabel()
+        label.text = title
+        label.font = UIFont.systemFont(ofSize: 16, weight: .medium)
+        label.textColor = .black
+        label.textAlignment = .left
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        // Stack view for image and label
+        let innerStackView = UIStackView(arrangedSubviews: [imageView, label])
+        innerStackView.axis = .horizontal
+        innerStackView.alignment = .center
+        innerStackView.spacing = 8
+        innerStackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        button.addSubview(innerStackView)
+        
+        NSLayoutConstraint.activate([
+            innerStackView.centerYAnchor.constraint(equalTo: button.centerYAnchor),
+            innerStackView.leadingAnchor.constraint(equalTo: button.leadingAnchor, constant: 8),
+            innerStackView.trailingAnchor.constraint(equalTo: button.trailingAnchor, constant: -8),
+            button.heightAnchor.constraint(equalToConstant: 32)
+        ])
+        
+        return button
     }
     
-    func updateButtonImage () {
-        // 현재 모드에 따라 배경 색 및 이미지 업데이트
-        if traitCollection.userInterfaceStyle == .light {
-            modeToggleButton.backgroundColor = .systemFill
-            modeToggleButton.setImage(UIImage(systemName: "moon.fill")?.withTintColor(.systemGray, renderingMode: .alwaysOriginal), for: .normal)
+}
 
-        } else if traitCollection.userInterfaceStyle == .dark {
-            modeToggleButton.backgroundColor = UIColor(red: 0.182, green: 0.219, blue: 0.261, alpha: 1)
-            modeToggleButton.setImage(UIImage(systemName: "sun.max.fill")?.withTintColor(.white, renderingMode: .alwaysOriginal), for: .normal)
-        }
+extension ViewController: UICollectionViewDelegateFlowLayout, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        4
     }
     
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = menuCollectionView.dequeueReusableCell(withReuseIdentifier: "MenuCell", for: indexPath) as! MenuItemCell
+        cell.configure(with: MenuItem.recommended_Menu[indexPath.item])
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 176, height: 212)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 0, left: 14, bottom: 0, right: 14)
+    }
     
 }
