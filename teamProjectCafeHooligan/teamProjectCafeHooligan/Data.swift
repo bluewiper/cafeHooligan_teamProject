36ea1c1ffc153dfdfb25bbe292a82932d382b738
RFClimber <choiwoolim98@gmail.com>
2024-07-05 14:04:59 +0900

    quantity 추가

--- teamProjectCafeHooligan/teamProjectCafeHooligan/Data.swift
+++ teamProjectCafeHooligan/teamProjectCafeHooligan/Data.swift
@@ -5,16 +5,17 @@ struct MenuItem {
     let imageName: String
     let menuName: String
     let menuPrice: Double
+    let quantity: Int
 }
 
 
 extension MenuItem{
     
     static let recommended_Menu = [
-        MenuItem(imageName: "cup.and.saucer", menuName: "coffee", menuPrice: 1.3),
-        MenuItem(imageName: "star", menuName: "cappuccino", menuPrice: 1.0),
-        MenuItem(imageName: "cup.and.saucer", menuName: "cafemocha", menuPrice: 0.8),
-        MenuItem(imageName: "star", menuName: "peachtea", menuPrice: 1.4),
+        MenuItem(imageName: "cup.and.saucer", menuName: "coffee", menuPrice: 1.3, quantity: 1),
+        MenuItem(imageName: "star", menuName: "cappuccino", menuPrice: 1.0, quantity: 1),
+        MenuItem(imageName: "cup.and.saucer", menuName: "cafemocha", menuPrice: 0.8, quantity: 1),
+        MenuItem(imageName: "star", menuName: "peachtea", menuPrice: 1.4, quantity: 1),
     ]
 }
 struct Category {
